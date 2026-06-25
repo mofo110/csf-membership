f82ade784fe893f44454ebbc9eaa9e6a54ca3dab
Morton Fojas <mortonfojas@Mortons-MacBook-Air.local>
2026-06-25 12:53:29 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# CSF Membership Active
+
+## Description
+
+This is my quick and dirty node app to:
+
+* Get CSF Membership from [CascadeShootingFacilities.org](https://www.cascadeshootingfacilities.org/membership/)
+* Send email notification of membership CLOSE or OPEN status
+
+## Requirements
+
+* Node
+* Dotenv
+* Nodemailer
+
+## References
+* Node.js Send an Email [W3Schools.com](https://www.w3schools.com/nodejs/nodejs_email.asp)
+* Nodemailer Well-Known Services [Nodemailer.com](https://nodemailer.com/smtp/well-known-services)
+* Fix #AUTH005 Error [CDESoftware.com](https://support.cdesoftware.com/kb/a3802/fixing-the-535-5_7_0-auth005-too-many-bad-auth-attempts-error.aspx)
+* Use Environment Variables [DEV.to](https://dev.to/siddharth151199/how-to-send-email-in-node-js-with-nodemailer-edb)
+
+## Usage
+
+### Step 1: Clone the repo and change to the working directory.
+
+```bash
+git clone https://github.com/mofo110/csf-membership.git
+cd csf-membership
+```
+
+### Step 2: Install dependencies.
+```bash
+npm install
+```
+
+### Step 3: Create .env file with Yahoo email username and password.
+```bash
+# .env
+EMAIL_USER=<your_yahoo_email>
+EMAIL_PASS=<your_yahoo_app_password>
+```
+
+### Step 4: Execute to get CSF Membership Link.
+
+```bash
+node get-csf-membership.js
+<p style="text-align: center;">Click Here
+Subject: CSF Membership is CLOSE!!!
+Text: No link found.
+◇ injected env (2) from .env // tip: ⌘ override existing { override: true }
+Email sent: 250 OK , completed
+```
---
+++ get-csf-membership.js
@@ -0,0 +1,94 @@
+/**
+ * CSF Membership page should have a link
+ *     https://www.cascadeshootingfacilities.org/membership/
+ */
+getCSFMembership();
+
+function getCSFMembership() {
+    const https = require('https');
+
+    const options = {
+        hostname: 'www.cascadeshootingfacilities.org',
+        port: 443,
+        path: '/membership/',
+        method: 'GET'
+    };
+
+    const req = https.request(options, (res) => {
+        let data = '';
+
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        res.on('end', () => {
+            getData(data);
+        });
+    });
+
+    req.on('error', (error) => {
+        console.error(error);
+    });
+
+    req.end();
+}
+
+function getData(data) {
+    const regex = new RegExp(/<h1>Interested in becoming a member\?<\/h1>(.*?)<\/p>/);
+    //const regex = new RegExp(/<h2>Renewal Information<\/h2>(.*?)<\/p>/);
+    const match = regex.exec(data.replaceAll('\n', ''));
+
+    if (match) {
+        const clickHere = match[1].trim();
+        getLink(clickHere);
+    } else {
+        console.log("No click here found.");
+    }
+}
+
+function getLink(clickHere) {
+    console.log(clickHere);
+
+    const regex = new RegExp(/href="(.*?)"/);
+    const match = regex.exec(clickHere.replaceAll('\n', ''));
+
+    let subject = 'CSF Membership is CLOSE!!!'
+    let text = 'No link found.';
+
+    if (match) {
+        subject = 'CSF Membership is OPEN!!!'
+        text = match[1].trim();
+    }
+    
+    email(subject, text)
+}
+
+function email(subject, text) {
+    console.log("Subject: " + subject);
+    console.log("Text: " + text);
+
+    require('dotenv').config();
+    const nodemailer = require('nodemailer');
+    const transporter = nodemailer.createTransport({
+        service: 'Yahoo',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_USER,
+        subject: subject,
+        text: text
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
